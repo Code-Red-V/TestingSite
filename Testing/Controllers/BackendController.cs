7932bc9c6debed795435a2db328675cc25846c05
VladXier <58219846+Code-Red-V@users.noreply.github.com>
2023-03-25 22:39:30 +0300

    Добавил BackendLayout, BackendController,в предстовления добавил папку Backend и Index это будет админка

---
+++ Testing/Controllers/BackendController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public class BackendController : Controller
+    {
+        private readonly ILogger<BackendController> _logger;
+        private readonly ApplicationContext app;
+
+        public BackendController(ILogger<BackendController> logger, ApplicationContext app)
+        {
+            _logger = logger;
+            this.app = app;
+        }
+
+        public IActionResult Index()
+        {
+            return View("/Views/Backend/Index.cshtml");
+        }
+    }
+}
