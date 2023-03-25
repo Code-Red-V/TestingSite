63eb8071e82b885d605f517ec1718a9d142cd1a6
VladXier <58219846+Code-Red-V@users.noreply.github.com>
2023-03-25 21:26:58 +0300

    Автоинкремент в таблице тест, новый хедер

--- Testing/Migrations/20230325173919_InitialCreate.cs
+++ Testing/Migrations/20230325173919_InitialCreate.cs
@@ -41,7 +41,8 @@ namespace Testing.Migrations
                 name: "Test",
                 columns: table => new
                 {
-                    TestId = table.Column<int>(type: "int", nullable: false),
+                    TestId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
                     Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: true),
                     Description = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
                     CategoryId = table.Column<int>(type: "int", nullable: true),
